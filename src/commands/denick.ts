07be294d0a443dd5087edebce8e2e9212c1623e6
Kamyx001 <Kamyx001@gmail.com>
2022-05-26 09:36:25 +0000

    fixed arm issiues

--- src/commands/denick.ts
+++ src/commands/denick.ts
@@ -17,45 +17,12 @@ export function denick(message: DiscordJS.Message, args: string[]): void {
             if ( !data.success ) {
                 msg.edit(`Failed to denick ${nick}`);
             }
-            // if ( !data.player.ign ) {
-            //     msg.edit(`Can't find a nick of player ${nick}`);
-            //     return;
-            // }
             if (data.success) {
                 if ( !data.player?.ign ) {
                     msg.edit(`Can't find a nick of player ${nick}`);
                     return;
                 }
-                const canvas = Canvas.createCanvas(400, 200);
-		        const context = canvas.getContext('2d');
-
-                const backgroundFile = await readFile('./background.jpg');
-                const background = new Canvas.Image();
-                background.src = backgroundFile;
-                
-                context.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-                context.font = '20px sans-serif';
-                context.fillStyle = '#ffffff';
-                context.fillText("nick:", 30, 40);
-
-                context.font = '30px sans-serif';
-                context.fillStyle = '#ff6f11';
-                context.fillText(nick, 30, 80);
-
-                context.font = '20px sans-serif';
-                context.fillStyle = '#ffffff';
-                context.fillText("IGN:", 30, 120);
-
-                context.font = '30px sans-serif';
-                context.fillStyle = '#ff504f';
-                context.fillText(data.player?.ign, 30, 160);
-
-
-                // Actually fill the text with a solid color
-
-                const attachment = new MessageAttachment(canvas.toBuffer('image/png'), 'profile-image.png');
-                msg.edit({ content: ` `, files: [attachment] });
+                msg.edit({ content: `${nick}'s ign is \`${data.player?.ign}\`` });
             } else {
                 msg.edit(`Failed to denick ${nick}`);
             }
