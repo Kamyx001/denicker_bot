6b26c34ab1aa5dc7db7a0e297a397c36a6a24b07
Mateusz Slomka <slomkam7@gmail.com>
2022-06-03 20:47:32 +0200

    refactor code

--- src/commands/denick.ts
+++ src/commands/denick.ts
@@ -1,29 +1,29 @@
-import DiscordJS, { MessageAttachment } from 'discord.js';
+import DiscordJS from 'discord.js';
+import { NickNameNotProvided } from '../errors/nicknameNotProvided';
 import { key } from '../main';
 
-export function denick(message: DiscordJS.Message, args: string[]): void {
-    const nick = args[2];
-    if (nick === undefined) {
-        message.reply("Please provide a nickname to denick");
+export async function denick(message: DiscordJS.Message, nickname: string ) {
+    if (nickname === undefined) {
+        throw new NickNameNotProvided();
+    }
+    
+    console.log(key);
+    const reply = await message.reply("Denicking...");
+    
+    const response = await fetch(`http://api.antisniper.net/denick?key=${key}&nick=${nickname}`);
+    const responseData = await response.json();
+
+    const { success } = responseData;
+
+    if ( !success ) {
+        await reply.edit(`Failed to denick ${nickname}`);
         return;
     }
-    console.log(key);
-    const denicked = fetch(`http://api.antisniper.net/denick?key=${key}&nick=${nick}`).then(data => data.json());
-    const reply = message.reply("Denicking...");
-    denicked.then(data => {
-        reply.then(async (msg) => {
-            if ( !data.success ) {
-                msg.edit(`Failed to denick ${nick}`);
-            }
-            if (data.success) {
-                if ( !data.player?.ign ) {
-                    msg.edit(`Can't find a nick of player ${nick}`);
-                    return;
-                }
-                msg.edit({ content: `${nick}'s ign is \`${data.player?.ign}\`` });
-            } else {
-                msg.edit(`Failed to denick ${nick}`);
-            }
-        });
-    });
+
+    if ( !responseData.player?.ign ) {
+        await reply.edit(`Can't find a nick of player ${nickname}`);
+        return;
+    }
+
+    await reply.edit({ content: `${nickname}'s ign is \`${responseData.player?.ign}\`` });
 }
---
+++ src/errors/nicknameNotProvided.ts
@@ -0,0 +1,8 @@
+
+
+
+export class NickNameNotProvided extends Error {
+  constructor() {
+    super('Nickname was not provided');
+  }
+}
